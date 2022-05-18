2e6080a988abb5c6f1c1687e3747c454de0693f3
«Aleksander <pavel4uk18@gmail.com>
2022-05-18 20:31:24 +0300

    task1ReadyCommit

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// # 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+//14212 -> нет
+//23432 -> да
+//12821 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string NumberStr = Console.ReadLine();
+
+int number = int.Parse(NumberStr);
+
+if ((number > 9999) && (number <= 99999))
+{
+    if (NumberStr[0]==NumberStr[4] && NumberStr[1]==NumberStr[3])
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+    else
+    {
+        Console.WriteLine("Введенное число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Введено некорректное значение");
+}
+
